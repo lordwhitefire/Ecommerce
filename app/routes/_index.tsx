0b42105e9b38297285a3b65aa0cd0e6296d47669
lordwhitefire <victormakuo2002@gmail.com>
2024-01-02 05:52:33 +0100

    edited the menu

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from 'react';
 import type { MetaFunction } from "@remix-run/node";
+import NavigationBar from '../components/NavigationBar';
+import BannerComponent from '../components/BannerComponent';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +11,39 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  // State for Exclusive dropdown in Navbar
+  const [isExclusiveOpen, setIsExclusiveOpen] = useState(false);
+
+  // State for Exclusive dropdown in Banner
+  const [bannerIsExclusiveOpen, setBannerIsExclusiveOpen] = useState(false);
+
+  // Toggle function for Exclusive dropdown
+  const toggleExclusiveDropdown = () => {
+    setIsExclusiveOpen(!isExclusiveOpen);
+    setBannerIsExclusiveOpen((prevBannerState) => !prevBannerState);
+  };
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <NavigationBar isExclusiveOpen={isExclusiveOpen} toggleExclusiveDropdown={toggleExclusiveDropdown} />
+      <BannerComponent bannerIsExclusiveOpen={bannerIsExclusiveOpen} setBannerIsExclusiveOpen={setBannerIsExclusiveOpen} />
+
+     {/* <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <img
+        src="https://drive.google.com/uc?export=view&id=1C3oSuF-HK9IlyTlo4gLYGym1LuejiFRS"
+        alt="Description of the image"
+        className="rounded-lg shadow-lg mt-4"
+      />*/}
     </div>
   );
 }
+
+
+
+
+
+
+
+
